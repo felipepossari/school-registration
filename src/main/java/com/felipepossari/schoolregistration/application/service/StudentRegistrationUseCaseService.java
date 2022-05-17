c0c868065b6a5d7de2c8b862aa3d0bf5e07eca13
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-17 09:39:37 -0300

    Implement create student method

--- src/main/java/com/felipepossari/schoolregistration/application/service/StudentRegistrationUseCaseService.java
+++ src/main/java/com/felipepossari/schoolregistration/application/service/StudentRegistrationUseCaseService.java
@@ -2,18 +2,27 @@ package com.felipepossari.schoolregistration.application.service;
 
 import com.felipepossari.schoolregistration.application.domain.Student;
 import com.felipepossari.schoolregistration.application.domain.StudentFilter;
+import com.felipepossari.schoolregistration.application.exception.EntityRegisteredException;
+import com.felipepossari.schoolregistration.application.exception.ErrorReason;
 import com.felipepossari.schoolregistration.application.port.in.StudentRegistrationUseCase;
+import com.felipepossari.schoolregistration.application.port.out.StudentRepositoryPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StudentRegistrationUseCaseService implements StudentRegistrationUseCase {
+
+    private final StudentRepositoryPort studentRepositoryPort;
+
     @Override
     public Student create(Student student) {
-        return null;
+        validateUniqueEmail(student.getEmail());
+        return studentRepositoryPort.create(student);
     }
 
     @Override
@@ -34,5 +43,12 @@ public class StudentRegistrationUseCaseService implements StudentRegistrationUse
     @Override
     public void delete(Long id) {
 
+    }
+
+    private void validateUniqueEmail(String email){
+        if(studentRepositoryPort.findByEmail(email).isPresent()){
+            log.warn(ErrorReason.EMAIL_ALREADY_REGISTERED.getMessage());
+            throw new EntityRegisteredException(ErrorReason.EMAIL_ALREADY_REGISTERED);
+        }
     }
 }
