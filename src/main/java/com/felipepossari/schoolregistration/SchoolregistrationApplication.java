884a1a5a23f54b7d0155e1608b6691bcfd9088fe
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-17 07:05:07 -0300

    Initial commit

---
+++ src/main/java/com/felipepossari/schoolregistration/SchoolregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.felipepossari.schoolregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolregistrationApplication.class, args);
+	}
+
+}
