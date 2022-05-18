d75a30c2e413087596378ff390f181c864fcdfc1
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 15:17:56 -0300

    Update overview api. Set default values for student and course id

--- src/main/java/com/felipepossari/schoolregistration/adapter/in/web/overview/v1/OverviewApi.java
+++ src/main/java/com/felipepossari/schoolregistration/adapter/in/web/overview/v1/OverviewApi.java
@@ -2,6 +2,7 @@ package com.felipepossari.schoolregistration.adapter.in.web.overview.v1;
 
 import com.felipepossari.schoolregistration.adapter.in.web.overview.v1.response.CourseOverviewResponse;
 import com.felipepossari.schoolregistration.adapter.in.web.overview.v1.response.StudentOverviewResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,24 +22,25 @@ public interface OverviewApi {
     String FALSE = "false";
     String PAGE_NUMBER = "0";
     String PAGE_SIZE = "20";
+    String ZERO = "0";
 
     @GetMapping({"", "/students"})
-    List<StudentOverviewResponse> getStudents(
+    ResponseEntity<List<StudentOverviewResponse>> getStudents(
             @RequestParam(name = PAGE, defaultValue = PAGE_NUMBER) int page,
             @RequestParam(name = SIZE, defaultValue = PAGE_SIZE) int size,
-            @RequestParam(name = STUDENT_ID, required = false) int studentId,
-            @RequestParam(name = COURSE_ID, required = false) int courseId,
+            @RequestParam(name = STUDENT_ID, defaultValue = ZERO) long studentId,
+            @RequestParam(name = COURSE_ID, defaultValue = ZERO) long courseId,
             @RequestParam(name = WITHOUT_ENROLLMENT,
                     defaultValue = FALSE,
                     required = false) boolean withoutEnrollment
     );
 
     @GetMapping("/courses")
-    List<CourseOverviewResponse> getCourses(
+    ResponseEntity<List<CourseOverviewResponse>> getCourses(
             @RequestParam(name = PAGE, defaultValue = PAGE_NUMBER) int page,
             @RequestParam(name = SIZE, defaultValue = PAGE_SIZE) int size,
-            @RequestParam(name = STUDENT_ID, required = false) int studentId,
-            @RequestParam(name = COURSE_ID, required = false) int courseId,
+            @RequestParam(name = STUDENT_ID, defaultValue = ZERO) long studentId,
+            @RequestParam(name = COURSE_ID, defaultValue = ZERO) long courseId,
             @RequestParam(name = WITHOUT_ENROLLMENT,
                     defaultValue = FALSE,
                     required = false) boolean withoutEnrollment
