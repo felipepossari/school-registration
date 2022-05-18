d5283a82a84d88d8245b43bede1ebaa96fa8b9f3
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 15:19:44 -0300

    Update use case to call find eager by filter in get cousers overview

--- src/main/java/com/felipepossari/schoolregistration/application/service/OverviewUseCaseService.java
+++ src/main/java/com/felipepossari/schoolregistration/application/service/OverviewUseCaseService.java
@@ -5,19 +5,27 @@ import com.felipepossari.schoolregistration.application.domain.CourseFilter;
 import com.felipepossari.schoolregistration.application.domain.Student;
 import com.felipepossari.schoolregistration.application.domain.StudentFilter;
 import com.felipepossari.schoolregistration.application.port.in.OverviewUseCase;
+import com.felipepossari.schoolregistration.application.port.out.CourseRepositoryPort;
+import com.felipepossari.schoolregistration.application.port.out.StudentRepositoryPort;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class OverviewUseCaseService implements OverviewUseCase {
+
+    private final StudentRepositoryPort studentRepositoryPort;
+    private final CourseRepositoryPort courseRepositoryPort;
+
     @Override
     public List<Student> getStudentOverview(StudentFilter filter) {
-        return null;
+        return List.of();
     }
 
     @Override
     public List<Course> getCoursesOverview(CourseFilter filter) {
-        return null;
+        return courseRepositoryPort.findEagerByFilter(filter);
     }
 }
