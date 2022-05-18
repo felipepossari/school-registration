3ded0e082b1770089359d9238154b96bf02505f6
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 06:13:45 -0300

    Post method implemented

--- src/main/java/com/felipepossari/schoolregistration/adapter/in/web/students/v1/StudentApiController.java
+++ src/main/java/com/felipepossari/schoolregistration/adapter/in/web/students/v1/StudentApiController.java
@@ -1,10 +1,14 @@
 package com.felipepossari.schoolregistration.adapter.in.web.students.v1;
 
+import com.felipepossari.schoolregistration.adapter.in.web.exception.InvalidRequestException;
 import com.felipepossari.schoolregistration.adapter.in.web.students.v1.request.StudentRequest;
 import com.felipepossari.schoolregistration.adapter.in.web.students.v1.response.StudentResponse;
+import com.felipepossari.schoolregistration.application.port.in.StudentRegistrationUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -13,11 +17,20 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class StudentApiController implements StudentApi {
+
+    private final StudentApiRequestValidator requestValidator;
+    private final StudentApiMapper mapper;
+    private final StudentRegistrationUseCase useCase;
+
     @Override
     public ResponseEntity<Void> post(StudentRequest studentRequest,
                                      BindingResult bindingResult,
                                      UriComponentsBuilder uriComponentsBuilder) {
-        return null;
+        validateRequest(bindingResult);
+        var student = mapper.toDomain(studentRequest);
+        student = useCase.create(student);
+        var location = uriComponentsBuilder.path("/{id}").buildAndExpand(student.getId());
+        return ResponseEntity.created(location.toUri()).build();
     }
 
     @Override
@@ -40,5 +53,14 @@ public class StudentApiController implements StudentApi {
     @Override
     public ResponseEntity<Void> delete(Long id) {
         return null;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) { webDataBinder.setValidator(requestValidator);}
+
+    private void validateRequest(BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new InvalidRequestException(bindingResult);
+        }
     }
 }
