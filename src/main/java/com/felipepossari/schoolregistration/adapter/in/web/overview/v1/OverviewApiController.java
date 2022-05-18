d5123206d4536f00a6751dd5545beb96829201a6
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 15:18:27 -0300

    Get course overview endpoint implemented

--- src/main/java/com/felipepossari/schoolregistration/adapter/in/web/overview/v1/OverviewApiController.java
+++ src/main/java/com/felipepossari/schoolregistration/adapter/in/web/overview/v1/OverviewApiController.java
@@ -2,27 +2,37 @@ package com.felipepossari.schoolregistration.adapter.in.web.overview.v1;
 
 import com.felipepossari.schoolregistration.adapter.in.web.overview.v1.response.CourseOverviewResponse;
 import com.felipepossari.schoolregistration.adapter.in.web.overview.v1.response.StudentOverviewResponse;
+import com.felipepossari.schoolregistration.application.port.in.OverviewUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class OverviewApiController implements OverviewApi {
+
+    private final OverviewUseCase overviewUseCase;
+    private final OverviewApiMapper mapper;
+
     @Override
-    public List<StudentOverviewResponse> getStudents(int page,
-                                                     int size,
-                                                     int studentId,
-                                                     int courseId,
-                                                     boolean withoutEnrollment) {
+    public ResponseEntity<List<StudentOverviewResponse>> getStudents(int page,
+                                                                     int size,
+                                                                     long studentId,
+                                                                     long courseId,
+                                                                     boolean withoutEnrollment) {
         return null;
     }
 
     @Override
-    public List<CourseOverviewResponse> getCourses(int page,
-                                                   int size,
-                                                   int studentId,
-                                                   int courseId,
-                                                   boolean withoutEnrollment) {
-        return null;
+    public ResponseEntity<List<CourseOverviewResponse>> getCourses(int page,
+                                                                   int size,
+                                                                   long studentId,
+                                                                   long courseId,
+                                                                   boolean withoutEnrollment) {
+        var filter = mapper.toFilter(page, size, studentId, courseId, withoutEnrollment);
+        var courses = overviewUseCase.getCoursesOverview(filter);
+        return ResponseEntity.ok(mapper.toCoursesResponse(courses));
     }
 }
