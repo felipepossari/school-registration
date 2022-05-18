3f990d852f57579e1e0bf7f98ce9d9953f4351d8
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 11:30:45 -0300

    Update unique key

--- src/main/resources/db/migration/V3__create-student-course-table.sql
+++ src/main/resources/db/migration/V3__create-student-course-table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `student_course` (
   `student_id` bigint NOT NULL,
   `course_id` bigint NOT NULL,
-  KEY `course_id` (`course_id`),
-  KEY `student_id` (`student_id`),
+  UNIQUE KEY `course_student_unique` (`student_id`,`course_id`),
   CONSTRAINT `course_fk` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`),
   CONSTRAINT `student_course_fk` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
