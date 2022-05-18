410a79d0d267d75572cce582829a6d985237abee
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 11:31:34 -0300

    Add configuration for object mapper

---
+++ src/main/java/com/felipepossari/schoolregistration/configuration/ObjectMapperConfig.java
@@ -0,0 +1,20 @@
+package com.felipepossari.schoolregistration.configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
+        return mapper;
+    }
+}
