622d42af72b1dfe6c26820a205eec3d2f7a4a57c
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 09:47:08 -0300

    Classes deleted

--- src/main/java/com/felipepossari/schoolregistration/adapter/in/out/mysql/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.felipepossari.schoolregistration.adapter.in.out.mysql.repository;
-
-import com.felipepossari.schoolregistration.adapter.in.out.mysql.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-}
