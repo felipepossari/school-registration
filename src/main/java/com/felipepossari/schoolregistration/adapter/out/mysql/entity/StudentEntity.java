2b3f9cfc71ff3eeb22074911b0bfac0c12110687
Felipe Possari <fcpfelipe@hotmail.com>
2022-05-18 11:32:53 -0300

    Add unique constraint annotation

--- src/main/java/com/felipepossari/schoolregistration/adapter/out/mysql/entity/StudentEntity.java
+++ src/main/java/com/felipepossari/schoolregistration/adapter/out/mysql/entity/StudentEntity.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.List;
 
 @Entity
@@ -34,8 +35,10 @@ public class StudentEntity {
 
     @ManyToMany
     @JoinTable(
-            name = "student_course", joinColumns = @JoinColumn(name = "student_id"),
-            inverseJoinColumns = @JoinColumn(name = "course_id")
+            name = "student_course",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"),
+            uniqueConstraints = {@UniqueConstraint(columnNames = {"student_id", "course_id"})}
     )
     private List<CourseEntity> courses;
 }
